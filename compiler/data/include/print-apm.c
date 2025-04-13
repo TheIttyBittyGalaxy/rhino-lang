c7e84f169426d7aa576b52e1e32868189e2513ba
TheIttyBittyGalaxy <43608508+TheIttyBittyGalaxy@users.noreply.github.com>
2025-04-13 23:30:31 +0100

    compiler: Bug fix assignment statement printing

--- compiler/data/include/print-apm.c
+++ compiler/data/include/print-apm.c
@@ -183,11 +183,11 @@ void PRINT_STATEMENT(Program *apm, size_t stmt_index, const char *source_text)
         break;
 
     case ASSIGNMENT_STATEMENT:
-        LAST_ON_LINE();
         PRINT("lhs: ");
         PRINT_EXPRESSION(apm, stmt->assignment_lhs, source_text);
         NEWLINE();
 
+        LAST_ON_LINE();
         PRINT("rhs: ");
         PRINT_EXPRESSION(apm, stmt->assignment_rhs, source_text);
         NEWLINE();
