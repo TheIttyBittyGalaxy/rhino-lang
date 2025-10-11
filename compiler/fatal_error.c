16295722bb91957fa8c9e58f5a5f12c7d03b9493
TheIttyBittyGalaxy <43608508+TheIttyBittyGalaxy@users.noreply.github.com>
2025-10-11 12:00:36 +0100

    compiler: Provide line and file with fatal error messages

--- compiler/fatal_error.c
+++ compiler/fatal_error.c
@@ -6,14 +6,14 @@
 
 extern bool flag_test_mode;
 
-void fatal_error(const char *message, ...)
+void fatal_error_at_line(const char *message, const char *file, int line, ...)
 {
     if (flag_test_mode)
     {
         printf("FATAL ERROR\n");
 
         va_list args;
-        va_start(args, message);
+        va_start(args, line);
         vprintf(message, args);
         va_end(args);
         printf("\n");
@@ -22,9 +22,10 @@ void fatal_error(const char *message, ...)
     }
 
     fprintf(stderr, "A fatal error has occurred. This is an issue with the Rhino compiler, and not with your program.\n");
+    fprintf(stderr, "%s:%d\n", file, line);
 
     va_list args;
-    va_start(args, message);
+    va_start(args, line);
     vfprintf(stderr, message, args);
     va_end(args);
     fprintf(stderr, "\n");
--- compiler/fatal_error.h
+++ compiler/fatal_error.h
@@ -1,6 +1,8 @@
 #ifndef FATAL_ERROR_H
 #define FATAL_ERROR_H
 
-void fatal_error(const char *message, ...);
+void fatal_error_at_line(const char *message, const char *file, int line, ...);
+
+#define fatal_error(message, ...) fatal_error_at_line(message, __FILE__, __LINE__, ##__VA_ARGS__)
 
 #endif
